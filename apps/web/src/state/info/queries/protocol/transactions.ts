43b60599cb9b02f0a400b6d6d2b9147abb1150df
son.hoang <son.hoang@sotatek.com>
2023-01-09 18:22:39 +0700

    edit

--- apps/web/src/state/info/queries/protocol/transactions.ts
+++ apps/web/src/state/info/queries/protocol/transactions.ts
@@ -2,6 +2,7 @@ import { gql } from 'graphql-request'
 import { mapBurns, mapMints, mapSwaps } from 'state/info/queries/helpers'
 import { BurnResponse, MintResponse, SwapResponse } from 'state/info/queries/types'
 import { Transaction, TransactionFrom } from 'state/info/types'
+import axios from 'axios'
 import { getMultiChainQueryEndPointWithStableSwap, MultiChainName } from '../../constant'
 
 /**
@@ -68,6 +69,68 @@ const GLOBAL_TRANSACTIONS = gql`
   }
 `
 
+const QUERYPANCAKE = `
+  query overviewTransactions {
+    mints: mints(first: 100, orderBy: timestamp, orderDirection: desc) {
+      id
+      timestamp
+      pair {
+        token0 {
+          id
+          symbol
+        }
+        token1 {
+          id
+          symbol
+        }
+      }
+      to
+      amount0
+      amount1
+      amountUSD
+    }
+    swaps: 
+    swaps(first: 100, orderBy: timestamp, orderDirection: desc) {
+      id
+      timestamp
+      pair {
+        token0 {
+          id
+          symbol
+        }
+        token1 {
+          id
+          symbol
+        }
+      }
+      sender
+      amount0In
+      amount1In
+      amount0Out
+      amount1Out
+      amountUSD
+    }
+    burns: burns(first: 100, orderBy: timestamp, orderDirection: desc) {
+      id
+      timestamp
+      pair {
+        token0 {
+          id
+          symbol
+        }
+        token1 {
+          id
+          symbol
+        }
+      }
+      sender
+      amount0
+      amount1
+      amountUSD
+    }
+  }
+`
+
 interface TransactionResults {
   mints: MintResponse[]
   swaps: SwapResponse[]
@@ -93,7 +156,6 @@ const fetchTopTransactions = async (
         return parseInt(b.timestamp, 10) - parseInt(a.timestamp, 10)
       })
     }
-
     if (chainName === 'ETH') {
       const dataUNI = await getMultiChainQueryEndPointWithStableSwap(
         chainName,
@@ -101,8 +163,6 @@ const fetchTopTransactions = async (
       ).request<TransactionResults>(GLOBAL_TRANSACTIONS)
 
       if (dataUNI) {
-        const mintsUNI = dataUNI.mints.map(mapMints)
-        const burnsUNI = dataUNI.burns.map(mapBurns)
         const swapsUNI = dataUNI.swaps.map(mapSwaps)
 
         transactionsOther = [...swapsUNI].sort((a, b) => {
@@ -110,15 +170,14 @@ const fetchTopTransactions = async (
         })
       }
     } else {
-      const dataPANCAKE = await getMultiChainQueryEndPointWithStableSwap(
-        chainName,
-        TransactionFrom.PANCAKE,
-      ).request<TransactionResults>(GLOBAL_TRANSACTIONS)
+      const dataPANCAKERES = await axios.post(`${process.env.NEXT_PUBLIC_API}/Pancake/query`, {url: 'https://proxy-worker-dev.pancake-swap.workers.dev/bsc-exchange',query: QUERYPANCAKE})
+      const dataPANCAKE = dataPANCAKERES?.data;
+      // const dataPANCAKE = await getMultiChainQueryEndPointWithStableSwap(
+      //   chainName,
+      //   TransactionFrom.PANCAKE,
+      // ).request<TransactionResults>(GLOBAL_TRANSACTIONS)
       if (dataPANCAKE) {
-        const mintsPANCAKE = dataPANCAKE.mints.map(mapMints)
-        const burnsPANCAKE = dataPANCAKE.burns.map(mapBurns)
         const swapsPANCAKE = dataPANCAKE.swaps.map(mapSwaps)
-
         transactionsOther = [...swapsPANCAKE].sort((a, b) => {
           return parseInt(b.timestamp, 10) - parseInt(a.timestamp, 10)
         })
