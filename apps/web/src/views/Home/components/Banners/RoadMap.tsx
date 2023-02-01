83221c8c69e3c3bae35873812011269bb2d9a453
son.hoang <son.hoang@sotatek.com>
2023-02-01 09:50:03 +0700

    edit

--- apps/web/src/views/Home/components/Banners/RoadMap.tsx
+++ apps/web/src/views/Home/components/Banners/RoadMap.tsx
@@ -114,7 +114,7 @@ const RoadMap = () => {
   const { width } = useWindowSize()
   return (
     <Wrapper style={{ overflow: 'hidden' }}>
-      <Title data-aos="fade-up">Road map</Title>
+      <Title data-aos="fade-up">Development map</Title>
       <Description data-aos="fade-up" data-aos-duration="2300">
         Building The Road For Web3 & Blockchain Mass Adoption
       </Description>
