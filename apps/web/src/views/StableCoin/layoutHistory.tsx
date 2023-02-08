7d740e10f726713ef699c9841b59d45ae443020a
son.hoang <son.hoang@sotatek.com>
2023-02-08 16:19:47 +0700

    edit

--- apps/web/src/views/StableCoin/layoutHistory.tsx
+++ apps/web/src/views/StableCoin/layoutHistory.tsx
@@ -86,8 +86,31 @@ const Container = styled.div`
   padding: 0px 48px;
   margin-bottom: 24px;
   color: rgba(0, 0, 0, 0.87);
+  justify-content: center;
+  @media (min-width: 1300px) {
+    .content {
+      width: 1400px;
+      margin: 0 auto;
+    }
+  }
   @media (max-width: 576px) {
     padding: 0px 24px;
+
+    .content {
+      width: unset;
+    }
+  }
+`
+const ContainerBanner = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 28px 0px 24px;
+
+  @media (max-width: 576px) {
+    padding: 28px 24px 24px;
+  }
+  .banner {
+    width: 1400px;
   }
 `
 
@@ -166,8 +189,17 @@ export default function LayoutHistory() {
   return (
     <>
       <MainBackground>{isMobile ? <SwapMainBackgroundMobile /> : <SwapMainBackgroundDesktop />}</MainBackground>
-      <InfoNav allTokens={allTokens} textContentBanner="Earn BUSD/USDC from Your  XOXS" />
+      <ContainerBanner>
+        <div className="banner">
+          <InfoNav
+            allTokens={allTokens}
+            textContentBanner="Earn BUSD/USDC from your XOXS Indefinitely"
+            hasPadding={false}
+          />
+        </div>
+      </ContainerBanner>
       <Container style={{marginBottom: 100}} key={`container-stablecoin${chainId}`}>
+        <div className='content'>
         <Flex alignItems="center" style={{ gap: 10 }}>
             <Flex
             onClick={() => setWidthDraw(TYPE.default)}
@@ -213,6 +245,7 @@ export default function LayoutHistory() {
               </Box>
             </WrapperBorder>
         </Row>
+        </div> 
       </Container>
     </>
   )
