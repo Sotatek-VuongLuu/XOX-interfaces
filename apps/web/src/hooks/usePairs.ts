a2ecfbbf6e85a9201f55f88e4eaee9bcd56a3b53
Quang Vuong <vuong.luu@sotatek.com>
2023-02-13 16:25:22 +0700

    fix: usePairXOX

--- apps/web/src/hooks/usePairs.ts
+++ apps/web/src/hooks/usePairs.ts
@@ -95,7 +95,7 @@ export function usePairXOX(): [PairState, Pair | null][] {
       if (!tokenA || !tokenB || tokenA.equals(tokenB)) return [PairState.INVALID, null]
       if (!reserves) return [PairState.NOT_EXISTS, null]
       const { reserve0, reserve1 } = reserves
-      const [token0, token1] = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA]
+      const [token0, token1] = [tokenA, tokenB]
       return [
         PairState.EXISTS,
         new Pair(
