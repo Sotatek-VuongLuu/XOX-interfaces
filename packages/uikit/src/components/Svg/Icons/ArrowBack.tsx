eb6f8ed269e4d3ef6de1204bc91b0c7cb94ae726
thanh.nguyen9 <thanh.nguyen9@sotatek.com>
2023-03-03 12:06:33 +0700

    fix: new design

--- packages/uikit/src/components/Svg/Icons/ArrowBack.tsx
+++ packages/uikit/src/components/Svg/Icons/ArrowBack.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 
-const Icon: React.FC = () => {
+const Icon = ({ width }: any) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <svg xmlns="http://www.w3.org/2000/svg" width={width} height="24" viewBox="0 0 24 24" fill="none">
       <path d="M6 11.9961H18" stroke="#515151" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-      <path d="M12 18L6 12L12 6" stroke="#515151" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" fill="none" />
+      <path
+        d="M12 18L6 12L12 6"
+        stroke="#515151"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        fill="none"
+      />
     </svg>
   );
 };
