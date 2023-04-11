b0bbeb6b340d32cda5c38222221495e220e616f6
Sotatek-ManhNguyen4 <manh.nguyen4@sotatek.com>
2023-04-11 14:37:01 +0700

    apply overlay

--- apps/web/src/views/Home/components/Banners/WelcomeXOX.tsx
+++ apps/web/src/views/Home/components/Banners/WelcomeXOX.tsx
@@ -66,32 +66,32 @@ const Wrapper = styled.div`
       }
     }
 
-    // .overlay {
-    //   height: 457px;
-    //   width: 457px;
-    //   border-radius: 50%;
-    //   position: absolute;
-    //   top: 0px;
-    //   left: 15%;
-    //   z-index: -2;
-    //   background: radial-gradient(
-    //     50% 50% at 50% 50%,
-    //     rgba(249, 124, 29, 0.5) 0%,
-    //     rgba(246, 99, 42, 0.5) 0.01%,
-    //     rgba(249, 124, 29, 0) 100%
-    //   );
-    //   opacity: 0.7;
-    //   scale: 1.3;
+    .overlay {
+      height: 500px;
+      width: 500px;
+      border-radius: 50%;
+      position: absolute;
+      top: 0px;
+      left: 0%;
+      z-index: -2;
+      background: radial-gradient(
+        50% 50% at 50% 50%,
+        rgba(249, 124, 29, 0.5) 0%,
+        rgba(246, 99, 42, 0.5) 0.01%,
+        rgba(249, 124, 29, 0) 100%
+      );
+      opacity: 0.7;
+      scale: 1.3;
 
-    //   @media screen and (max-width: 900px) {
-    //     width: 100%;
-    //     left: unset;
-    //   }
+      @media screen and (max-width: 900px) {
+        width: 100%;
+        left: unset;
+      }
 
-    //   @media screen and (max-width: 576px) {
-    //     height: 230px;
-    //   }
-    // }
+      @media screen and (max-width: 576px) {
+        height: 230px;
+      }
+    }
   }
 
   @media screen and (max-width: 900px) {
@@ -1017,7 +1017,7 @@ const WelcomeXOX = (): JSX.Element => {
           </GridLeft>
           <Grid item xs={12} md={5} sx={{ height: '300px', minHeight: '300px', overflow: 'visible' }}>
             <div className="video-container">
-              {/* <div className="overlay"></div> */}
+              <div className="overlay"></div>
               <video
                 autoPlay
                 loop
