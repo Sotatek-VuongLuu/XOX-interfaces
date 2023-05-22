40452618113013d07cee806dfc8467ea217ae397
thanh.nguyen9 <thanh.nguyen9@sotatek.com>
2023-05-22 16:28:13 +0700

    fix: wrong network

--- apps/web/src/components/Logo/ChainLogo.tsx
+++ apps/web/src/components/Logo/ChainLogo.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/future/image'
 import { HelpIcon } from '@pancakeswap/uikit'
-import { isChainSupported } from 'utils/wagmi'
+import { isBridgeChainSupported, isChainSupported } from 'utils/wagmi'
 import { memo } from 'react'
 
 export const ChainLogo = memo(
   ({ chainId, width = 24, height = 24 }: { chainId: number; width?: number; height?: number }) => {
-    if (isChainSupported(chainId)) {
+    if (isBridgeChainSupported(chainId)) {
       return (
         <Image
           alt={`chain-${chainId}`}
