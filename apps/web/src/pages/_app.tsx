203e85237bd4945231d137f62daab0892fddf2ef
thanh.nguyen9 <thanh.nguyen9@sotatek.com>
2023-05-29 16:42:27 +0700

    change: site color

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -86,6 +86,7 @@ function MyApp(props: AppProps<{ initialReduxState: any }>) {
           content="width=device-width, initial-scale=1, maximum-scale=5, minimum-scale=1, viewport-fit=cover"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="theme-color" content="#000" />
       </Head>
       <Providers store={store}>
         <PageMeta />
