cdcd36f433c1738d3a231d8d59241df81e0caf42
Sotatek-KienDao <kien.dao@sotatek.com>
2023-07-19 14:41:51 +0700

    change xoxToken address tbsc

--- apps/web/src/config/constants/contracts.ts
+++ apps/web/src/config/constants/contracts.ts
@@ -201,7 +201,7 @@ export default {
     [ChainId.OPTIMISM_TESTNET]: '0x27D2ec949C4D93D6246E538eBCACf1A76d7A899E', // TODO: change address
   },
   xoxToken: {
-    [ChainId.BSC_TESTNET]: '0xC82f6DF626787D0FD686Ed211A232Fe8Cd1EF42d',
+    [ChainId.BSC_TESTNET]: '0x6bb15Fd179539BFD6E78f18f5d91543142e0Ad9e',
     [ChainId.GOERLI]: '0x92b3d85b4589bBEE8e3a04114C217Aa497B44aDD',
     [ChainId.ETHEREUM]: '', // TODO: change address
     [ChainId.BSC]: '',
